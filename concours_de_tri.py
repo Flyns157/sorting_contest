d7a85a3f0b05788ee6cf0b80421277d2c39cc0ea
Mattéo Cuisset <matteo.cuisset@gmail.com>
2024-02-03 17:33:20 +0100

    Disqualification added if sorting fails.

--- concours_de_tri.py
+++ concours_de_tri.py
@@ -2,7 +2,11 @@ import time
 import random
 import importlib
 import sys
+import types
 import pandas as pd
+
+# Initialiser le DataFrame pour stocker les scores
+scores = pd.DataFrame(columns=['Algorithme', 'Temps d\'exécution', 'Statut'])
 
 # Importer les algorithmes de tri du module sort_pack
 sys.path.insert(0, 'sort_pack')
@@ -15,20 +19,18 @@ def generate_random_list(size):
 # Vérifier si une liste est triée
 def is_sorted(lst):
     return all(lst[i] <= lst[i+1] for i in range(len(lst)-1))
-import types
-# Initialiser le DataFrame pour stocker les scores
-scores = pd.DataFrame(columns=['Algorithme', 'Temps d\'exécution'])
 
 # Tester un algorithme de tri
 def test_sorting_algorithm(algorithm, lst):
     if isinstance(algorithm, types.FunctionType):  # Vérifier que l'attribut est une fonction
         start_time = time.time()
-        sorted_lst = algorithm(lst)
+        sorted_lst = algorithm(lst[:])
+        # print(lst,algorithm.__name__,sorted_lst) #DEBUG
         end_time = time.time()
         execution_time = end_time - start_time
         if is_sorted(sorted_lst):
-            return execution_time
-    return None  # L'attribut n'est pas une fonction ou l'algorithme est disqualifié
+            return execution_time, 'Qualifié'
+    return None, 'Disqualifié'  # L'attribut n'est pas une fonction ou l'algorithme est disqualifié
 
 # Le concours
 def contest():
@@ -39,16 +41,14 @@ def contest():
     for algorithm in dir(sort_pack):
         if algorithm.startswith('__'):
             continue
-        execution_time = test_sorting_algorithm(getattr(sort_pack, algorithm), lst)
-        if execution_time is not None:
-            # Ajouter le score à la DataFrame
-            scores.loc[len(scores)] = [algorithm, execution_time]
-        else:
-            print(f"L'algorithme {algorithm} est disqualifié ou n'est pas une fonction.")
+        execution_time, status = test_sorting_algorithm(getattr(sort_pack, algorithm), lst)
+        # Ajouter le score et le statut à la DataFrame
+        scores.loc[len(scores)] = [algorithm, execution_time, status]
 
     # Trier la DataFrame par temps d'exécution et attribuer les rangs
-    scores.sort_values(by='Temps d\'exécution', inplace=True)
+    scores.sort_values(by='Temps d\'exécution', inplace=True, na_position='last')
     scores['Rang'] = scores['Temps d\'exécution'].rank(method='min')
+
     print(scores)
 
 # Lancer le concours
